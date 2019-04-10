85d210c76510ac09cde8494b38190d2ea51398c0
Anya51 <fabio.rodrigues@dcomp.ufs.br>
2019-04-10 16:08:47 -0300

    graph

--- instanceHandling.c
+++ instanceHandling.c
@@ -14,6 +14,29 @@ typedef struct Instances
     int budget;
 } Instance;
 
+typedef struct adjacencyNodes
+{   
+    int idNext;
+    adjacencyNode* next;
+    /* data */
+}adjacencyNode;
+
+typedef struct adjacencyLists
+{
+    adjacencyList* head;
+    /* data */
+}adjacencyList;
+
+typedef struct Graphs
+{
+    /* data */
+    int nNodes;
+    adjacencyList* array; 
+}Graph;
+
+
+
+
 int errno;
 
 // Implemented Functions
