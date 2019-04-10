f0b5f8b4e91cbde8efd2004f30e32a6edb4f69fc
Anya51 <43707299+Anya51@users.noreply.github.com>
2019-04-10 13:36:07 -0300

    start

---
+++ instanceHandling.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+typedef struct Instances
+{
+    char name[120];
+    int nVar, nClauses;
+    int **clauses;
+    int *mandatory;
+    int *cost;
+    int *benefits;
+    int budget;
+} Instance;
+
+int errno;
+
+// Implemented Functions
+Instance *handleInstance (char *fileName);
+static void closeFile (FILE *fp);
+static FILE *openFileRead (char *fileName, char *mode);
+void printInstance (Instance* instance);
+static void exitApplication(Instance* instance);
+
+// Test Unit
+int main(int argc, char **argv)
+{
+
+    Instance* instance = NULL;
+    instance = handleInstance("instance.txt");
+    printInstance(instance);
+    exitApplication(instance);
+    return 0;
+}
+
+// Function receives a char* fileName  and a char* mode
+// Return a non-NULL pointer file.
+static FILE *openFileRead(char *fileName, char *mode)
+{
+    FILE *fp = NULL;
+    if ((fp = fopen(fileName, mode)) == NULL)
+    {
+        fprintf(stderr, "File cannot been opened.");
+    }
+    return fp;
+}
+
+// Function receives a FILE* fp and close the FILE*
+// Return nothing
+static void closeFile(FILE *fp)
+{
+    fclose(fp);
+    return;
+}
+
+// Function that receives a char * fileName
+// Return a Intance * intance
+Instance *handleInstance(char *fileName)
+{
+    Instance *instance = NULL;
+    FILE *input = NULL;
+    int aux = 0;
+
+    input = openFileRead("instance.txt", "r");
+    instance = malloc(sizeof(instance));
+
+    fgets(instance->name, 120, input);
+    fscanf(input, " %d %d", &instance->nVar, &instance->nClauses);
+
+    instance->clauses =  malloc(sizeof(int*) * instance->nClauses * sizeof(instance->clauses));
+
+    for (int i = 0; i < instance->nClauses; i++)
+    {
+
+        fscanf(input, " %1d", &aux);
+        instance->clauses[i] =  malloc(sizeof(int) * sizeof(instance->clauses[i]) * (aux + 100));
+        instance->clauses[i][0] = aux;
+
+        for (int j = 1; j <= aux; j++)
+        {
+
+            fscanf(input, " %d", &instance->clauses[i][j]);
+            printf("%d ", instance->clauses[i][j]);
+
+        }
+            printf("\n");
+    }
+
+    fscanf(input, " %1d", &aux);
+    instance->mandatory =  malloc(sizeof(int*) * aux);
+    instance->mandatory[0] = aux;
+
+    for (int j = 1; j <= aux; j++)
+    {
+
+        fscanf(input, " %d", &instance->mandatory[j]);
+        printf("%d ", instance->mandatory[j]);
+
+    }
+    printf("\n");
+
+//!!
+    instance->cost =  malloc(sizeof(int*) * instance->nVar);
+
+    for (int j = 0; j < instance->nVar; j++)
+    {
+
+        fscanf(input, " %d", &instance->cost[j]);
+        printf("%d ", instance->cost[j]);
+
+    }
+    printf("\n");
+
+    instance->benefits =  malloc(sizeof(int*) * instance->nVar);
+
+    for (int j = 0; j < instance->nVar; j++)
+    {
+
+        fscanf(input, " %d", &instance->benefits[j]);
+        printf("%d ", instance->benefits[j]);
+
+    }
+    printf("\n");
+    fscanf(input, " %d", &instance->budget);
+    printf("%d ", instance->budget);
+    printf("\n\n");
+    closeFile(input);
+    return instance;
+}
+
+void printInstance (Instance* instance)
+{
+
+    for (int i = 0; i < instance->nClauses; i++)
+    {
+        for (int j = 1; j <= instance->clauses[i][0]; j++)
+        {
+
+            printf("%d ", instance->clauses[i][j]);
+
+        }
+            printf("\n");
+    }
+
+    for (int j = 1; j <= instance->mandatory[0]; j++)
+    {
+
+        printf("%d ", instance->mandatory[j]);
+
+    }
+    printf("\n");
+
+    for (int j = 0; j < instance->nVar; j++)
+    {
+
+        printf("%d ", instance->cost[j]);
+
+    }
+    printf("\n");
+
+    for (int j = 0; j < instance->nVar; j++)
+    {
+        printf("%d ", instance->benefits[j]);
+    }
+    printf("\n");
+    printf("%d ", instance->budget);
+    printf("\n\n");
+
+
+    return;
+}
+
+static void exitApplication(Instance* instance)
+{
+    free(instance->clauses);
+    free(instance);
+
+    instance->clauses = NULL;
+    instance = NULL;
+
+    return;
+}
